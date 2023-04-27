1f24222403454486351bebf499443f81b2dd0d9b
lucastribioli <luk.tribioli.nunes@gmail.com>
2023-04-27 13:21:12 -0300

    create function addAumentoSalario

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,6 +1,14 @@
+import java.math.BigDecimal
+import java.math.RoundingMode
+
 fun main(args: Array<String>) {
-    testesArraysInt()
-    testesArraysDouble()
+//    testesArraysInt()
+//    testesArraysDouble()
+   val salarios = bigDecimalArrayOf("100.03", "444343.888", "60000")
+//    println(salarios.contentToString())
+
+    addAumentoSalario(salarios)
+
 }
 
 fun testesArraysInt(){
@@ -17,7 +25,7 @@ fun testesArraysInt(){
 
 fun testesArraysDouble(){
     val salarios = doubleArrayOf(1000.5, 3344.5, 4554.3, 3432.4, 9448.5, 9033.4)
-    var aumento = 1.1
+    val aumento = 1.1
 
     salarios.forEachIndexed { index, salario ->
         salarios[index] = salario * aumento
@@ -25,3 +33,25 @@ fun testesArraysDouble(){
 
     println(salarios.contentToString())
 }
+
+fun bigDecimalArrayOf(vararg valores: String): Array<BigDecimal>{
+    return Array<BigDecimal>(valores.size){i ->
+            valores[i].toBigDecimal()
+    }
+}
+
+fun addAumentoSalario(salarios: Array<BigDecimal>){
+    val aumento = "1.1".toBigDecimal()
+    val salariosComAumento = salarios
+        .map { salario ->
+            if (salario < "5000".toBigDecimal()) {
+                (salario * aumento).setScale(2, RoundingMode.UP)
+            }else{
+                salario + "500".toBigDecimal()
+            }
+        }
+        .toTypedArray()
+
+    println(salariosComAumento.contentToString())
+}
+
