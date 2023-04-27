06fa363073a2b3319648214fdcd62f8bf3d3546d
lucastribioli <luk.tribioli.nunes@gmail.com>
2023-04-27 12:17:04 -0300

    create function testesArraysDouble

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,4 +1,9 @@
 fun main(args: Array<String>) {
+    testesArraysInt()
+    testesArraysDouble()
+}
+
+fun testesArraysInt(){
     val idades = intArrayOf(10, 33, 44, 32, 98, 90)
     var maiorIdade = Int.MIN_VALUE
     idades.forEach {idade ->
@@ -8,4 +13,15 @@ fun main(args: Array<String>) {
     }
 
     println(maiorIdade)
+}
+
+fun testesArraysDouble(){
+    val salarios = doubleArrayOf(1000.5, 3344.5, 4554.3, 3432.4, 9448.5, 9033.4)
+    var aumento = 1.1
+
+    salarios.forEachIndexed { index, salario ->
+        salarios[index] = salario * aumento
+    }
+
+    println(salarios.contentToString())
 }
